c8ee09271fc3fcba40994787d25c8d23ac82f5a3
Ramom Karllos <rkzeraa@gmail.com>
2023-03-03 16:55:44 -0300

    Start Maven Project in CMD

---
+++ src/main/java/br/rkdev/HelloWorld.java
@@ -0,0 +1,14 @@
+package br.rkdev;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args ) {
+
+        Parser parser = new Parser();
+        parser.message();
+    }
+}
---
+++ src/test/java/br/rkdev/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package br.rkdev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HelloWorld.
+ */
+public class HelloWorldTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
